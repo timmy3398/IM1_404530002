986cb8de96101937ff437095a96b4094721c296b
HanselHu <apologize0616@gmail.com>
2016-03-16 00:58:40 +0800

    作業批改完成!

--- rectangleTester.java
+++ rectangleTester.java
@@ -1,64 +1,69 @@
+﻿/*
+ * [A]100
+ * [TA's advise]
+ * 1.width,height有沒有可能是浮點數呢?
+ * 2.Good Job!
+ * */
+
 package main;
-  
-  class Rectangle{
-	  
-	 
-	   int width, height, x, y;
-	
-	  Rectangle(){}
-	  
-	  
-	  Rectangle(int _height, int _width, int _x, int _y){
-	 
-		  this.height =_height;
-	  this.width= _width;
-	  this.x = _x;
-	  this.y = _y;}
-	  
-	  int getHeight(){ return this.height;}
-	  
-	  
-	  int getWidth(){return this.width;}
-	  
-	 
-	  public String toString(){
-		  
-		  return "java.Rectangle[x=" + x + ",y=" + y + ",width=" + width + ",height=" + height + "]";
-	  }
-		  
-  }
-	  
-  public class rectangleTester {
- 
-	  public static void main(String[] args){
-	  
-		  double Area,Perimeter;
-		  
-		  Rectangle a= new  Rectangle(18, 57, 15, 25);
-		  Rectangle b= new  Rectangle(47, 60, 0, 12);
-		  
-		  
-		  Perimeter = (a.getWidth() + a.getHeight()+a.getWidth() + a.getHeight());
-		  
-		  Area = a.getWidth() * a.getHeight();
-		  	
-		   		System.out.println(a.toString());
-		   		System.out.println("Area = " + Area);
-		   		System.out.println("Perimeter = " + Perimeter );
-		   		
-		   	 System.out.println("\n");
-		   		Perimeter = (b.getWidth() + b.getHeight()+b.getWidth()+b.getHeight());
-		   		
-		   		Area = b.getWidth() * b.getHeight();
-		   	 	
-		   	  		System.out.println(b.toString());
-		   	  		System.out.println("Area = " + Area);
-		   	  	System.out.println("Perimeter = " + Perimeter); 
-		   	
-		   	 System.out.println("\n");
-		   	  	
-		   	  	System.out.println("End of Output!");
-  }  
- 
-  } 
-	  
+
+class Rectangle {
+
+	int width, height, x, y;
+
+	Rectangle() {
+		// this(-1,-1,-1,-1);//加上這行初始化物件會讓你的程式更完整.
+	}
+
+	Rectangle(int _height, int _width, int _x, int _y) {
+		this.height = _height;
+		this.width = _width;
+		this.x = _x;
+		this.y = _y;
+	}
+
+	int getHeight() {
+		return this.height;
+	}
+
+	int getWidth() {
+		return this.width;
+	}
+
+	public String toString() {
+		return "java.Rectangle[x=" + x + ",y=" + y + ",width=" + width + ",height=" + height + "]";
+	}
+}
+
+public class rectangleTester {
+
+	public static void main(String[] args) {
+
+		double Area, Perimeter;
+
+		Rectangle a = new Rectangle(18, 57, 15, 25);
+		Rectangle b = new Rectangle(47, 60, 0, 12);
+
+		Perimeter = (a.getWidth() + a.getHeight() + a.getWidth() + a.getHeight());
+
+		Area = a.getWidth() * a.getHeight();
+
+		System.out.println(a.toString());
+		System.out.println("Area = " + Area);
+		System.out.println("Perimeter = " + Perimeter);
+
+		System.out.println("\n");
+		Perimeter = (b.getWidth() + b.getHeight() + b.getWidth() + b.getHeight());
+
+		Area = b.getWidth() * b.getHeight();
+
+		System.out.println(b.toString());
+		System.out.println("Area = " + Area);
+		System.out.println("Perimeter = " + Perimeter);
+
+		System.out.println("\n");
+
+		System.out.println("End of Output!");
+	}
+
+}
